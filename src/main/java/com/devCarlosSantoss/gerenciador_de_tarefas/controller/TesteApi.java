0df6fb3e69a388d1d77f58950961cd0b68bbd03a
devCarlosSantoss <202103485529@alunos.estacio.br>
2026-04-15 18:58:22 -0300

    feat: add new route

--- src/main/java/com/devCarlosSantoss/gerenciador_de_tarefas/controller/TesteApi.java
+++ src/main/java/com/devCarlosSantoss/gerenciador_de_tarefas/controller/TesteApi.java
@@ -1,6 +1,8 @@
 package com.devCarlosSantoss.gerenciador_de_tarefas.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +11,10 @@ public class TesteApi {
     @GetMapping
     private String teste(){
         return "Teste";
+    }
+
+    @GetMapping("/teste")
+    public ResponseEntity<String> testeDeString(@RequestParam(name = "nome") String nome){
+        return ResponseEntity.ok("Carlos " + nome);
     }
 }
